f13ec9a05d87ad9cd33661d06cbb96ccb4a2a018
Majdi <nassib.haouari@gmail.com>
2018-11-05 22:52:47 +0100

    Les directives par attribut

--- src/app/appareil/appareil.component.ts
+++ src/app/appareil/appareil.component.ts
@@ -19,4 +19,12 @@ export class AppareilComponent implements OnInit {
     return this.appareilStatus;
   }
 
+  getColor() {
+    if(this.appareilStatus === 'allumé') {
+      return 'green';
+    } else if(this.appareilStatus === 'éteint') {
+      return 'red';
+    }
+  }
+
 }
