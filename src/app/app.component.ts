90a39fbca73f7239f99af3f2d4c1aa54830e60a8
Majdi <nassib.haouari@gmail.com>
2018-11-05 22:39:36 +0100

    Les directives structurelles

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,9 +8,21 @@ import { Component } from '@angular/core';
 export class AppComponent {
   isAuth = false;
 
-  appareilOne = 'Machine à laver';
-  appareilTwo = 'Frigo';
-  appareilThree = 'Ordinateur';
+  appareils = [
+    {
+      name: 'Machine à laver',
+      status: 'éteint'
+    },
+    {
+      name: 'Frigo',
+      status: 'allumé'
+    },
+    {
+      name: 'Ordinateur',
+      status: 'éteint'
+    }
+  ];
+
 
 
   constructor() {
