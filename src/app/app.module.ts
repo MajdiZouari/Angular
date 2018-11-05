cce17f5aa138ff8ac738d4dd1ac027a52b20dbf5
Majdi <nassib.haouari@gmail.com>
2018-11-05 16:34:59 +0100

    Mon premier component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { MonPremierComponent } from './mon-premier/mon-premier.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MonPremierComponent
   ],
   imports: [
     BrowserModule,
