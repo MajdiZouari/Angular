a2b93f4585e6047bd8b52732ea29bcbef4935422
Majdi <nassib.haouari@gmail.com>
2018-11-06 01:37:50 +0100

    Le Pipe async

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,7 +8,14 @@ import { Component } from '@angular/core';
 export class AppComponent {
   isAuth = false;
 
-  lastUpdate = new Date();
+  lastUpdate = new Promise((resolve, reject) => {
+    const date = new Date();
+    setTimeout(
+      () => {
+        resolve(date);
+      }, 2000
+    );
+  });
 
   appareils = [
     {
