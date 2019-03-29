7b9a1ac67d1dd7aa88d1fe647c274ab6cf7c8232
Majdi <nassib.haouari@gmail.com>
2019-03-29 20:01:46 +0100

    Routing avec des parametres

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -10,11 +10,13 @@ import { AuthComponent } from './auth/auth.component';
 import { AppareilViewComponent } from './appareil-view/appareil-view.component';
 import { RouterModule, Routes } from "@angular/router";
 import { Authservice } from "./services/auth.service";
+import { SingleAppareilComponent } from './single-appareil/single-appareil.component';
 
 
 
 const appRoutes: Routes = [
   { path: 'appareils', component: AppareilViewComponent },
+  { path: 'appareils/:id', component: SingleAppareilComponent },
   { path: 'auth', component: AuthComponent },
   { path: '', component: AppareilViewComponent}
 ];
@@ -25,7 +27,8 @@ const appRoutes: Routes = [
     MonPremierComponent,
     AppareilComponent,
     AuthComponent,
-    AppareilViewComponent
+    AppareilViewComponent,
+    SingleAppareilComponent
   ],
   imports: [
     BrowserModule,
--- src/app/services/appareil.service.ts
+++ src/app/services/appareil.service.ts
@@ -2,18 +2,30 @@ export class AppareilService{
 
   appareils = [
     {
+      id: 1,
       name: 'Machine à laver',
       status: 'éteint'
     },
     {
+      id: 2,
       name: 'Frigo',
       status: 'allumé'
     },
     {
+      id: 3,
       name: 'Ordinateur',
       status: 'éteint'
     }
   ];
+
+  getAppareilById(id: number){
+    const appareil = this.appareils.find(
+      (appareillObject) => {
+        return appareillObject.id === id;
+      }
+    );
+    return appareil;
+  }
 
   switchOnAll() {
     for(let appareil of this.appareils) {
